d884941bcce59d09e5f2a39fbfc7ac3bff753964
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2022-10-28 10:45:14 +0200

    Improve testing of fusion datamodel and bump file sizes of new CKB fields

--- datamodel/src/test/java/com/hartwig/serve/datamodel/fusion/FusionPairComparatorTest.java
+++ datamodel/src/test/java/com/hartwig/serve/datamodel/fusion/FusionPairComparatorTest.java
@@ -5,21 +5,18 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import com.google.common.collect.Lists;
-import com.hartwig.serve.datamodel.common.ProteinEffect;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 public class FusionPairComparatorTest {
 
     @Test
     public void canSortFusionPairs() {
-        FusionPair pair1 = create("A", "B", 2, 3);
-        FusionPair pair2 = create("A", "B", null, null);
-        FusionPair pair3 = create("A", "C", null, null);
-        FusionPair pair4 = create("B", "C", null, null);
-        FusionPair pair5 = create("X", "A", null, null);
+        FusionPair pair1 = FusionTestFactory.createFusionPair("A", "B", 2, 3);
+        FusionPair pair2 = FusionTestFactory.createFusionPair("A", "B", null, null);
+        FusionPair pair3 = FusionTestFactory.createFusionPair("A", "C", null, null);
+        FusionPair pair4 = FusionTestFactory.createFusionPair("B", "C", null, null);
+        FusionPair pair5 = FusionTestFactory.createFusionPair("X", "A", null, null);
 
         List<FusionPair> fusionPairs = Lists.newArrayList(pair4, pair1, pair5, pair3, pair2);
         fusionPairs.sort(new FusionPairComparator());
@@ -29,53 +26,5 @@ public class FusionPairComparatorTest {
         assertEquals(pair3, fusionPairs.get(2));
         assertEquals(pair4, fusionPairs.get(3));
         assertEquals(pair5, fusionPairs.get(4));
-    }
-
-    @NotNull
-    private static FusionPair create(@NotNull String geneUp, @NotNull String geneDown, @Nullable Integer minExonUp,
-            @Nullable Integer minExonDown) {
-        return new FusionPair() {
-            @NotNull
-            @Override
-            public String geneUp() {
-                return geneUp;
-            }
-
-            @Nullable
-            @Override
-            public Integer minExonUp() {
-                return minExonUp;
-            }
-
-            @Nullable
-            @Override
-            public Integer maxExonUp() {
-                return null;
-            }
-
-            @NotNull
-            @Override
-            public String geneDown() {
-                return geneDown;
-            }
-
-            @Nullable
-            @Override
-            public Integer minExonDown() {
-                return minExonDown;
-            }
-
-            @Nullable
-            @Override
-            public Integer maxExonDown() {
-                return null;
-            }
-
-            @NotNull
-            @Override
-            public ProteinEffect proteinEffect() {
-                return ProteinEffect.UNKNOWN;
-            }
-        };
     }
 }
--- datamodel/src/test/java/com/hartwig/serve/datamodel/fusion/FusionTestFactory.java
+++ datamodel/src/test/java/com/hartwig/serve/datamodel/fusion/FusionTestFactory.java
@@ -1,15 +1,24 @@
 package com.hartwig.serve.datamodel.fusion;
 
+import java.util.Objects;
+
 import com.hartwig.serve.datamodel.DatamodelTestFactory;
 import com.hartwig.serve.datamodel.Knowledgebase;
 import com.hartwig.serve.datamodel.common.ProteinEffect;
 
 import org.apache.logging.log4j.util.Strings;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class FusionTestFactory {
 
     private FusionTestFactory() {
+    }
+
+    @NotNull
+    public static FusionPair createFusionPair(@NotNull String geneUp, @NotNull String geneDown, @Nullable Integer minExonUp,
+            @Nullable Integer minExonDown) {
+        return new FusionPairImpl(geneUp, minExonUp, null, geneDown, minExonDown, null, ProteinEffect.UNKNOWN);
     }
 
     @NotNull
@@ -34,5 +43,103 @@ public final class FusionTestFactory {
     @NotNull
     public static ActionableFusion createTestActionableFusionForSource(@NotNull Knowledgebase source) {
         return actionableFusionBuilder().source(source).build();
+    }
+
+    private static class FusionPairImpl implements FusionPair {
+
+        @NotNull
+        private final String geneUp;
+        @Nullable
+        private final Integer minExonUp;
+        @Nullable
+        private final Integer maxExonUp;
+        @NotNull
+        private final String geneDown;
+        @Nullable
+        private final Integer minExonDown;
+        @Nullable
+        private final Integer maxExonDown;
+        @NotNull
+        private final ProteinEffect proteinEffect;
+
+        public FusionPairImpl(@NotNull final String geneUp, @Nullable final Integer minExonUp, @Nullable final Integer maxExonUp,
+                @NotNull final String geneDown, @Nullable final Integer minExonDown, @Nullable final Integer maxExonDown,
+                @NotNull final ProteinEffect proteinEffect) {
+            this.geneUp = geneUp;
+            this.minExonUp = minExonUp;
+            this.maxExonUp = maxExonUp;
+            this.geneDown = geneDown;
+            this.minExonDown = minExonDown;
+            this.maxExonDown = maxExonDown;
+            this.proteinEffect = proteinEffect;
+        }
+
+        @NotNull
+        @Override
+        public String geneUp() {
+            return geneUp;
+        }
+
+        @Nullable
+        @Override
+        public Integer minExonUp() {
+            return minExonUp;
+        }
+
+        @Nullable
+        @Override
+        public Integer maxExonUp() {
+            return maxExonUp;
+        }
+
+        @NotNull
+        @Override
+        public String geneDown() {
+            return geneDown;
+        }
+
+        @Nullable
+        @Override
+        public Integer minExonDown() {
+            return minExonDown;
+        }
+
+        @Nullable
+        @Override
+        public Integer maxExonDown() {
+            return maxExonDown;
+        }
+
+        @NotNull
+        @Override
+        public ProteinEffect proteinEffect() {
+            return proteinEffect;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            final FusionPairImpl that = (FusionPairImpl) o;
+            return geneUp.equals(that.geneUp) && Objects.equals(minExonUp, that.minExonUp) && Objects.equals(maxExonUp, that.maxExonUp)
+                    && geneDown.equals(that.geneDown) && Objects.equals(minExonDown, that.minExonDown) && Objects.equals(maxExonDown,
+                    that.maxExonDown) && proteinEffect == that.proteinEffect;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(geneUp, minExonUp, maxExonUp, geneDown, minExonDown, maxExonDown, proteinEffect);
+        }
+
+        @Override
+        public String toString() {
+            return "FusionPairImpl{" + "geneUp='" + geneUp + '\'' + ", minExonUp=" + minExonUp + ", maxExonUp=" + maxExonUp + ", geneDown='"
+                    + geneDown + '\'' + ", minExonDown=" + minExonDown + ", maxExonDown=" + maxExonDown + ", proteinEffect=" + proteinEffect
+                    + '}';
+        }
     }
 }

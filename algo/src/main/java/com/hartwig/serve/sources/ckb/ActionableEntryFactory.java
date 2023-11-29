ff413e368d5db8d1a15652a262d3b31a9d7cbed9
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-11-29 11:59:22 +0100

    ACTIN-68: Add interface for actionable entry factory

---
+++ algo/src/main/java/com/hartwig/serve/sources/ckb/ActionableEntryFactory.java
@@ -0,0 +1,13 @@
+package com.hartwig.serve.sources.ckb;
+
+import java.util.Set;
+
+import com.hartwig.serve.ckb.datamodel.CkbEntry;
+
+import org.jetbrains.annotations.NotNull;
+
+interface ActionableEntryFactory {
+
+    @NotNull
+    Set<ActionableEntry> create(@NotNull CkbEntry entry, @NotNull String sourceGene, @NotNull String sourceEvent);
+}

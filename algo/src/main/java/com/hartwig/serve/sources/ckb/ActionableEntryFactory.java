5c17599e3d0246018c9c036992690c781778461b
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2023-12-11 16:59:43 +0100

    ACTIN-68: Added line break

--- algo/src/main/java/com/hartwig/serve/sources/ckb/ActionableEntryFactory.java
+++ algo/src/main/java/com/hartwig/serve/sources/ckb/ActionableEntryFactory.java
@@ -7,6 +7,7 @@ import com.hartwig.serve.ckb.datamodel.CkbEntry;
 import org.jetbrains.annotations.NotNull;
 
 interface ActionableEntryFactory {
+
     @NotNull
     Set<ActionableEntry> create(@NotNull CkbEntry entry, @NotNull String sourceEvent, @NotNull String sourceGene);
 }

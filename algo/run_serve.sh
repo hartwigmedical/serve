747b8cb0f5360c89a233ff9d2f3175efa65b1a57
Matthijs van Niekerk <m.vanniekerk@hartwigmedicalfoundation.nl>
2024-03-01 14:13:29 +0100

    INFRA-249 fix entrypoint

--- algo/run_serve.sh
+++ algo/run_serve.sh
@@ -2,8 +2,8 @@
 
 set -eo pipefail
 
-# fail if less than 2 args are provided
-if [ "$#" -ne 2 ]; then
+# fail if not exactly one argument is given
+if [ "$#" -ne 1 ]; then
     echo "Unzips the ckb input, then runs serve on it."
     echo "Usage: $0 <version>"
     exit 1
@@ -13,5 +13,8 @@ version=$1
 ckb_path="/data/resources/custom/ckb/production"
 
 mkdir -p $ckb_path
+echo "Unzipping ckb input... from /in/ckb/api-export-$version.zip to $ckb_path/api-export"
 unzip -o "/in/ckb/api-export-$version.zip" -d $ckb_path > /dev/null
+echo "Unzipped ckb input."
+echo "Running serve... with ckb path $ckb_path/api-export"
 java -jar /opt/app/serve.jar -properties_file /opt/app/serve.properties -ckb_dir "$ckb_path/api-export"

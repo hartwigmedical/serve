6166f61f755ae9fe07f2e212d12da40da4df99e7
John Bartlett <j.bartlett@hartwigmedicalfoundation.nl>
2024-05-22 17:48:00 -0400

    ACTIN-975: Parallelize JSON conversion and build lookup maps

--- ckb-importer/src/main/java/com/hartwig/serve/ckb/datamodel/variant/VariantFactory.java
+++ ckb-importer/src/main/java/com/hartwig/serve/ckb/datamodel/variant/VariantFactory.java
@@ -1,6 +1,8 @@
 package com.hartwig.serve.ckb.datamodel.variant;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
 import com.hartwig.serve.ckb.datamodel.reference.ReferenceFactory;
@@ -16,72 +18,74 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class VariantFactory {
+    @NotNull
+    private final CkbJsonDatabase ckbJsonDatabase;
+    @NotNull
+    private final Map<Integer, JsonVariant> variantsById;
+    @NotNull
+    private final Map<Integer, JsonGene> genesById;
 
-    private VariantFactory() {
+    public VariantFactory(@NotNull CkbJsonDatabase ckbJsonDatabase) {
+        this.ckbJsonDatabase = ckbJsonDatabase;
+        variantsById = ckbJsonDatabase.variants().stream().collect(Collectors.toMap(JsonVariant::id, variant -> variant));
+        genesById = ckbJsonDatabase.genes().stream().collect(Collectors.toMap(JsonGene::id, gene -> gene));
     }
 
     @NotNull
-    public static List<Variant> extractVariants(@NotNull CkbJsonDatabase ckbJsonDatabase, @NotNull List<VariantInfo> variantInfos) {
-        List<Variant> variants = Lists.newArrayList();
-        for (VariantInfo variantInfo : variantInfos) {
-            variants.add(resolveVariant(ckbJsonDatabase, variantInfo));
-        }
-
-        return variants;
+    public List<Variant> extractVariants(@NotNull List<VariantInfo> variantInfos) {
+        return variantInfos.stream().map(this::resolveVariant).collect(Collectors.toList());
     }
 
     @NotNull
-    private static Variant resolveVariant(@NotNull CkbJsonDatabase ckbJsonDatabase, @NotNull VariantInfo variantInfo) {
-        for (JsonVariant variant : ckbJsonDatabase.variants()) {
-            if (variant.id() == variantInfo.id()) {
-                return ImmutableVariant.builder()
-                        .id(variant.id())
-                        .createDate(variant.createDate())
-                        .updateDate(variant.updateDate())
-                        .fullName(variant.fullName())
-                        .variant(variant.variant())
-                        .impact(variant.impact())
-                        .proteinEffect(variant.proteinEffect())
-                        .type(variant.type())
-                        .associatedWithDrugResistance(variant.associatedWithDrugResistance())
-                        .transformingActivity(variant.transformingActivity())
-                        .polymorphism(variant.polymorphism())
-                        .hotspotReference(variant.hotspotReference())
-                        .isHotspot(variant.hotspotReference() != null)
-                        .gene(resolveGene(ckbJsonDatabase, variant.gene()))
-                        .referenceTranscriptCoordinate(convertReferenceTranscriptCoordinate(variant.referenceTranscriptCoordinate()))
-                        .allTranscriptCoordinates(convertAllTranscriptCoordinates(variant.allTranscriptCoordinates()))
-                        .categoryVariantPaths(convertCategoryVariantPaths(variant.categoryVariantPaths()))
-                        .memberVariants(convertMemberVariants(variant.memberVariants()))
-                        .description(ReferenceFactory.extractDescription("variant", variant.id(), variant.descriptions()))
-                        .references(ReferenceFactory.extractDescriptionReferences(ckbJsonDatabase, variant.descriptions()))
-                        .build();
-            }
+    private Variant resolveVariant(@NotNull VariantInfo variantInfo) {
+        if (variantsById.containsKey(variantInfo.id())) {
+            JsonVariant variant = variantsById.get(variantInfo.id());
+            return ImmutableVariant.builder()
+                    .id(variant.id())
+                    .createDate(variant.createDate())
+                    .updateDate(variant.updateDate())
+                    .fullName(variant.fullName())
+                    .variant(variant.variant())
+                    .impact(variant.impact())
+                    .proteinEffect(variant.proteinEffect())
+                    .type(variant.type())
+                    .associatedWithDrugResistance(variant.associatedWithDrugResistance())
+                    .transformingActivity(variant.transformingActivity())
+                    .polymorphism(variant.polymorphism())
+                    .hotspotReference(variant.hotspotReference())
+                    .isHotspot(variant.hotspotReference() != null)
+                    .gene(resolveGene(variant.gene()))
+                    .referenceTranscriptCoordinate(convertReferenceTranscriptCoordinate(variant.referenceTranscriptCoordinate()))
+                    .allTranscriptCoordinates(convertAllTranscriptCoordinates(variant.allTranscriptCoordinates()))
+                    .categoryVariantPaths(convertCategoryVariantPaths(variant.categoryVariantPaths()))
+                    .memberVariants(convertMemberVariants(variant.memberVariants()))
+                    .description(ReferenceFactory.extractDescription("variant", variant.id(), variant.descriptions()))
+                    .references(ReferenceFactory.extractDescriptionReferences(ckbJsonDatabase, variant.descriptions()))
+                    .build();
         }
 
         throw new IllegalStateException("Could not resolve CKB variant with id '" + variantInfo.id() + "'");
     }
 
     @NotNull
-    private static Gene resolveGene(@NotNull CkbJsonDatabase ckbJsonDatabase, @NotNull GeneInfo geneInfo) {
-        for (JsonGene gene : ckbJsonDatabase.genes()) {
-            if (gene.id() == geneInfo.id()) {
-                return ImmutableGene.builder()
-                        .id(gene.id())
-                        .createDate(gene.createDate())
-                        .updateDate(gene.updateDate())
-                        .geneSymbol(gene.geneSymbol())
-                        .geneRole(gene.geneRole())
-                        .entrezId(gene.entrezId())
-                        .chromosome(gene.chromosome())
-                        .mapLocation(gene.mapLocation())
-                        .canonicalTranscript(gene.canonicalTranscript())
-                        .terms(gene.terms())
-                        .synonyms(gene.synonyms())
-                        .description(ReferenceFactory.extractDescription("gene", gene.id(), gene.descriptions()))
-                        .references(ReferenceFactory.extractDescriptionReferences(ckbJsonDatabase, gene.descriptions()))
-                        .build();
-            }
+    private Gene resolveGene(@NotNull GeneInfo geneInfo) {
+        if (genesById.containsKey(geneInfo.id())) {
+            JsonGene gene = genesById.get(geneInfo.id());
+            return ImmutableGene.builder()
+                    .id(gene.id())
+                    .createDate(gene.createDate())
+                    .updateDate(gene.updateDate())
+                    .geneSymbol(gene.geneSymbol())
+                    .geneRole(gene.geneRole())
+                    .entrezId(gene.entrezId())
+                    .chromosome(gene.chromosome())
+                    .mapLocation(gene.mapLocation())
+                    .canonicalTranscript(gene.canonicalTranscript())
+                    .terms(gene.terms())
+                    .synonyms(gene.synonyms())
+                    .description(ReferenceFactory.extractDescription("gene", gene.id(), gene.descriptions()))
+                    .references(ReferenceFactory.extractDescriptionReferences(ckbJsonDatabase, gene.descriptions()))
+                    .build();
         }
 
         throw new IllegalStateException("Could not resolve CKB gene with id '" + geneInfo.id() + "'");
@@ -98,13 +102,7 @@ public final class VariantFactory {
 
     @NotNull
     private static List<TranscriptCoordinate> convertAllTranscriptCoordinates(@NotNull List<JsonTranscriptCoordinate> coordinates) {
-        List<TranscriptCoordinate> allTranscriptCoordinates = Lists.newArrayList();
-
-        for (JsonTranscriptCoordinate coordinate : coordinates) {
-            allTranscriptCoordinates.add(convertTranscriptCoordinate(coordinate));
-        }
-
-        return allTranscriptCoordinates;
+        return coordinates.stream().map(VariantFactory::convertTranscriptCoordinate).collect(Collectors.toList());
     }
 
     @NotNull
@@ -122,13 +120,7 @@ public final class VariantFactory {
 
     @NotNull
     private static List<String> convertCategoryVariantPaths(@NotNull List<JsonCategoryVariantPath> jsonCategoryVariantPaths) {
-        List<String> categoryVariantPaths = Lists.newArrayList();
-
-        for (JsonCategoryVariantPath categoryVariantPath : jsonCategoryVariantPaths) {
-            categoryVariantPaths.add(categoryVariantPath.variantPath());
-        }
-
-        return categoryVariantPaths;
+        return jsonCategoryVariantPaths.stream().map(JsonCategoryVariantPath::variantPath).collect(Collectors.toList());
     }
 
     @NotNull

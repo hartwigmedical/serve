2efc5cc5179bed5efb026ea9ffc02978d20b47a7
Charlotte Bruel <c.bruel@hartwigmedicalfoundation.nl>
2024-08-08 10:34:44 +0200

    ACTIN-1166: Improve new evidenceDirection naming

--- datamodel/src/main/java/com/hartwig/serve/datamodel/EvidenceDirection.java
+++ datamodel/src/main/java/com/hartwig/serve/datamodel/EvidenceDirection.java
@@ -1,35 +1,35 @@
 package com.hartwig.serve.datamodel;
 
 public enum EvidenceDirection {
-    RESPONSIVE(true, false, true, false),
+    RESPONSIVE(true, false, false, true),
     PREDICTED_RESPONSIVE(true, false, false, false),
-    DECREASED_RESPONSE(true, false, true, true),
-    NO_BENEFIT(false, false, true, false),
-    RESISTANT(false, true, true, false),
-    PREDICTED_RESISTANT(false, true, false, false);
+    DECREASED_RESPONSE(false, false, false, true),
+    NO_BENEFIT(false, true, false, true),
+    RESISTANT(false, true, true, true),
+    PREDICTED_RESISTANT(false, true, true, false);
 
-    private final boolean isResponsive;
+    private final boolean hasPositiveResponse;
+    private final boolean hasNoBenefit;
     private final boolean isResistant;
     private final boolean isCertain;
-    private final boolean isLessResponsive;
 
-    EvidenceDirection(final boolean isResponsive, final boolean isResistant, final boolean isCertain, final boolean isLessResponsive) {
-        this.isResponsive = isResponsive;
+    EvidenceDirection(final boolean hasPositiveResponse, final boolean hasNoBenefit, final boolean isResistant, final boolean isCertain) {
+        this.hasPositiveResponse = hasPositiveResponse;
+        this.hasNoBenefit = hasNoBenefit;
         this.isResistant = isResistant;
         this.isCertain = isCertain;
-        this.isLessResponsive = isLessResponsive;
     }
 
-    public boolean isResponsive() {
-        return isResponsive;
+    public boolean hasPositiveResponse() {
+        return hasPositiveResponse;
+    }
+
+    public boolean hasNoBenefit() {
+        return hasNoBenefit;
     }
 
     public boolean isResistant() {
         return isResistant;
-    }
-
-    public boolean isLessResponsive() {
-        return isLessResponsive;
     }
 
     @SuppressWarnings("unused")

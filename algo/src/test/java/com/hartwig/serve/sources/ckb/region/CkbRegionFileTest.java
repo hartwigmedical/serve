c6a51df6490281574d796320666d4ea2d8327430
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-08-17 10:22:44 +0200

    ACTIN-1032: Formatting

--- algo/src/test/java/com/hartwig/serve/sources/ckb/region/CkbRegionFileTest.java
+++ algo/src/test/java/com/hartwig/serve/sources/ckb/region/CkbRegionFileTest.java
@@ -1,13 +1,12 @@
 package com.hartwig.serve.sources.ckb.region;
 
-import static com.hartwig.serve.sources.ckb.CkbTestFactory.createLocation;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 
 import com.google.common.io.Resources;
+import com.hartwig.serve.sources.ckb.CkbTestFactory;
 
 import org.junit.Test;
 
@@ -19,13 +18,11 @@ public class CkbRegionFileTest {
         CkbRegion netherlands = regions.stream().filter(r -> r.country().equals("netherlands")).findFirst().orElseThrow();
         CkbRegion us = regions.stream().filter(r -> r.country().equals("united states")).findFirst().orElseThrow();
 
-        assertTrue(netherlands.includes(createLocation("Netherlands", null, "Rotterdam", "Noord Holland", null)));
-        assertTrue(netherlands.includes(createLocation("Netherlands", null, "Rotterdam", null, null)));
+        assertTrue(netherlands.includes(CkbTestFactory.createLocation("Netherlands", null, "Rotterdam", "Noord Holland", null)));
+        assertTrue(netherlands.includes(CkbTestFactory.createLocation("Netherlands", null, "Rotterdam", null, null)));
 
-        assertTrue(us.includes(createLocation("United States", null, "Augusta", "Maine", null)));
-        assertFalse(us.includes(createLocation("United States", null, "LA", "California", null)));
-        assertFalse(us.includes(createLocation("United States", null, "New York", null, null)));
-
+        assertTrue(us.includes(CkbTestFactory.createLocation("United States", null, "Augusta", "Maine", null)));
+        assertFalse(us.includes(CkbTestFactory.createLocation("United States", null, "LA", "California", null)));
+        assertFalse(us.includes(CkbTestFactory.createLocation("United States", null, "New York", null, null)));
     }
-
 }

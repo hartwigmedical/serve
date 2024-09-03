5c8ee7ee13379df5670c1e8c25f9ded488e78855
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-09-03 13:40:13 -0400

    ACTIN-1217: reorder for consistency

--- algo/src/main/java/com/hartwig/serve/sources/ckb/ActionableEvidenceFactory.java
+++ algo/src/main/java/com/hartwig/serve/sources/ckb/ActionableEvidenceFactory.java
@@ -141,6 +141,7 @@ class ActionableEvidenceFactory implements ActionableEntryFactory {
 
                     actionableEntries.add(ImmutableActionableEntry.builder()
                             .source(Knowledgebase.CKB_EVIDENCE)
+                            .date(entry.updateDate())
                             .sourceEvent(sourceEvent)
                             .sourceUrls(sourceUrls)
                             .intervention(ImmutableTreatment.builder()
@@ -150,12 +151,11 @@ class ActionableEvidenceFactory implements ActionableEntryFactory {
                                     .build())
                             .applicableCancerType(cancerTypeExtraction.applicableCancerType())
                             .blacklistCancerTypes(cancerTypeExtraction.blacklistedCancerTypes())
+                            .description(evidence.efficacyEvidence())
                             .level(level)
                             .approvalStatus(approvalStatus)
                             .direction(direction)
                             .evidenceUrls(evidenceUrls)
-                            .date(entry.updateDate())
-                            .description(evidence.efficacyEvidence())
                             .build());
                 }
             }

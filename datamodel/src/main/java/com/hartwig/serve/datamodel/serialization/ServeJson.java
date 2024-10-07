409c40860206c65caa5f1d87e3e2300d60b7d3d1
Khalid Zuberi <k.zuberi@hartwigmedicalfoundation.nl>
2024-10-07 13:59:41 -0400

    ACTIN-1320: format dates to match our style elsewhere

--- datamodel/src/main/java/com/hartwig/serve/datamodel/serialization/ServeJson.java
+++ datamodel/src/main/java/com/hartwig/serve/datamodel/serialization/ServeJson.java
@@ -4,10 +4,20 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.hartwig.serve.datamodel.RefGenome;
 import com.hartwig.serve.datamodel.ServeRecord;
@@ -15,8 +25,13 @@ import com.hartwig.serve.datamodel.ServeRecord;
 import org.jetbrains.annotations.NotNull;
 
 public final class ServeJson {
+
     private static final JsonMapper MAPPER = JsonMapper.builder()
             .addModule(new JavaTimeModule())
+            .addModule(new SimpleModule()
+                    .addSerializer(LocalDate.class, new LocalDateSerializer())
+                    .addDeserializer(LocalDate.class, new LocalDateDeserializer())
+            )
             .serializationInclusion(JsonInclude.Include.NON_NULL)
             .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
             .build();
@@ -42,5 +57,34 @@ public final class ServeJson {
     @NotNull
     public static String jsonFilePath(@NotNull String outputDir, @NotNull RefGenome refGenome) {
         return refGenome.addVersionToFilePath(outputDir + "/serve.json");
+    }
+
+    static class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+        public LocalDateSerializer() {
+            super(LocalDate.class);
+        }
+
+        @Override
+        public void serialize(LocalDate date, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            Map<String, Integer> dateMap = new LinkedHashMap<>();
+            dateMap.put("year", date.getYear());
+            dateMap.put("month", date.getMonthValue());
+            dateMap.put("day", date.getDayOfMonth());
+            gen.writeObject(dateMap);
+        }
+    }
+
+    static class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+        @Override
+        public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            Map<String, Integer> dateMap = p.readValueAs(Map.class);
+            Integer year = dateMap.get("year");
+            Integer month = dateMap.get("month");
+            Integer day = dateMap.get("day");
+
+            return LocalDate.of(year, month, day);
+        }
     }
 }

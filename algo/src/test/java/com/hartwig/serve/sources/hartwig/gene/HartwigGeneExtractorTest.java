13603390df34323d6d38d37516b656cbc1b055ba
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-11-12 15:07:31 +0100

    ACTIN-1457: Switch expected and actual in tests

--- algo/src/test/java/com/hartwig/serve/sources/hartwig/gene/HartwigGeneExtractorTest.java
+++ algo/src/test/java/com/hartwig/serve/sources/hartwig/gene/HartwigGeneExtractorTest.java
@@ -21,10 +21,9 @@ public class HartwigGeneExtractorTest {
         ExtractionResult result = victim.extract(List.of(ImmutableHartwigGeneEntry.builder().gene(GENE).geneRole("onco").build()));
         assertEquals(result.knownGenes().size(), 1);
         KnownGene knownGene = result.knownGenes().iterator().next();
-        assertEquals(knownGene.gene(), GENE);
-        assertEquals(knownGene.geneRole(), GeneRole.ONCO);
-        assertEquals(knownGene.sources().size(), 1);
-        assertEquals(knownGene.sources().iterator().next(), Knowledgebase.HARTWIG_GENE_CURATED);
+        assertEquals(GENE, knownGene.gene());
+        assertEquals(GeneRole.ONCO, knownGene.geneRole());
+        assertEquals(1, knownGene.sources().size());
+        assertEquals(Knowledgebase.HARTWIG_GENE_CURATED, knownGene.sources().iterator().next());
     }
-
 }

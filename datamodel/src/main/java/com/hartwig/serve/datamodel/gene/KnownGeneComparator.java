1ea553760323f3a285515ca03550c1bcbe1adece
Korneel Duyvesteyn <k.duyvesteyn@hartwigmedicalfoundation.nl>
2024-11-13 16:27:59 +0100

    ACTIN-1466: Include gene role in known genes comparator (#49)

--- datamodel/src/main/java/com/hartwig/serve/datamodel/gene/KnownGeneComparator.java
+++ datamodel/src/main/java/com/hartwig/serve/datamodel/gene/KnownGeneComparator.java
@@ -8,6 +8,11 @@ public class KnownGeneComparator implements Comparator<KnownGene> {
 
     @Override
     public int compare(@NotNull KnownGene gene1, @NotNull KnownGene gene2) {
-        return gene1.gene().compareTo(gene2.gene());
+        int geneCompare = gene1.gene().compareTo(gene2.gene());
+        if (geneCompare != 0) {
+            return geneCompare;
+        }
+
+        return gene1.geneRole().compareTo(gene2.geneRole());
     }
 }

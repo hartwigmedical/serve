b385bc953a9ad4739de95634776c0a4d1450a041
Nina Jacobs <n.jacobs@hartwigmedicalfoundation.nl>
2025-02-19 18:50:13 +0100

    Remove superfluous 'where' criteria in actionableTrials view

--- algo/src/main/resources/database/generate_serve_views.sql
+++ algo/src/main/resources/database/generate_serve_views.sql
@@ -5,25 +5,18 @@ SELECT nctId, group_concat(DISTINCT sourceEvent ORDER BY sourceEvent SEPARATOR '
     INNER JOIN trialMolecularCriterium ON actionableTrial.id=trialMolecularCriterium.actionableTrialId
     INNER JOIN (
         SELECT molecularCriteriumId, sourceEvent FROM actionableCharacteristic
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableCodon
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableExon
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId from trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableFusion
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableGene
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableHla
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
             UNION
         SELECT molecularCriteriumId, sourceEvent FROM actionableHotspot
-        WHERE molecularCriteriumId IN (SELECT molecularCriteriumId FROM trialMolecularCriterium)
     ) AS result ON trialMolecularCriterium.molecularCriteriumId=result.molecularCriteriumId
 GROUP BY 1
 ORDER BY 2

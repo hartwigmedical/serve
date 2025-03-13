c7e6de388c9ef8f3b19e2586a4d9add3e550038b
Paul Wolfe <p.wolfe@hartwigmedicalfoundation.nl>
2025-03-13 11:03:46 +0100

    ACTIN-1596 Add phases to the evidence level details datamodel
    Just adds the phases to the existing enum and adds logic to the factory
    
    Decided not to change the name, as it would break backwards compatibility of SERVE.
    I think for next major serve update the name "EvidenceApprovalStage" would be an
    improvement over "EvidenceLevelDetails"

--- datamodel/src/main/java/com/hartwig/serve/datamodel/efficacy/EvidenceLevelDetails.java
+++ datamodel/src/main/java/com/hartwig/serve/datamodel/efficacy/EvidenceLevelDetails.java
@@ -5,8 +5,14 @@ public enum EvidenceLevelDetails {
     PRECLINICAL,
     CASE_REPORTS_SERIES,
     CLINICAL_STUDY,
+    PHASE_0,
+    PHASE_I,
+    PHASE_IB_II,
+    PHASE_II,
+    PHASE_III,
     FDA_APPROVED,
     GUIDELINE,
     FDA_CONTRAINDICATED,
+    
     UNKNOWN
 }
